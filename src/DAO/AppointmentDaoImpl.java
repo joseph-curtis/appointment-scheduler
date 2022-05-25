a2a396530a11f22d4bb96a0735807ed71a8f310f
Joseph Curtis <jcur175@wgu.edu>
2022-05-24 23:18:49 -0700

    implemented SQL methods

--- src/DAO/AppointmentDaoImpl.java
+++ src/DAO/AppointmentDaoImpl.java
@@ -15,6 +15,7 @@
 
 package DAO;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import model.Appointment;
 import model.User;
@@ -34,8 +35,23 @@ public class AppointmentDaoImpl extends DataAccessObject<Appointment, User> {
      */
     @Override
     public ObservableList<Appointment> getAll() throws SQLException {
+        ObservableList<Appointment> appointmentsList = FXCollections.observableArrayList();
 
-        return null;    // TODO implement method
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement statement = conn.prepareStatement(
+                     """
+                             SELECT * FROM client_schedule.appointments\s
+                             JOIN customers\s
+                                  ON customers.Customer_ID = appointments.Customer_ID\s
+                             JOIN contacts\s
+                                  ON contacts.Contact_ID = appointments.Contact_ID
+                             """)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                appointmentsList.add(createRecordFromResultSet(resultSet));
+            }
+        }
+        return appointmentsList;
     }
 
     /**
@@ -43,24 +59,22 @@ public class AppointmentDaoImpl extends DataAccessObject<Appointment, User> {
      */
     @Override
     public Optional<Appointment> getById(int id) throws SQLException {
-        ResultSet resultSet = null;
-
-        // TODO:  join customers and contacts tables
-
         try (Connection conn = dataSource.getConnection();
              PreparedStatement statement = conn.prepareStatement(
-                     "SELECT * FROM APPOINTMENTS WHERE Appointment_ID = ?")) {
+                 """
+                         SELECT * FROM APPOINTMENTS\s
+                         JOIN customers\s
+                              ON customers.Customer_ID = appointments.Customer_ID\s
+                         JOIN contacts\s
+                              ON contacts.Contact_ID = appointments.Contact_ID
+                         WHERE Appointment_ID = ?
+                         """)) {
             statement.setInt(1, id);
-            resultSet = statement.executeQuery();
+            ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
                 return Optional.of(createRecordFromResultSet(resultSet));
             } else {
                 return Optional.empty();
-            }
-        }
-        finally {
-            if (resultSet != null) {
-                resultSet.close();
             }
         }
     }
@@ -69,7 +83,7 @@ public class AppointmentDaoImpl extends DataAccessObject<Appointment, User> {
      * {@inheritDoc}
      */
     @Override
-    public boolean add(Appointment appointment, User currentUser) throws SQLException {
+    public boolean add(Appointment appointment, User user) throws SQLException {
         if (getById(appointment.id()).isPresent()) {
             return false;
         }
@@ -78,42 +92,13 @@ public class AppointmentDaoImpl extends DataAccessObject<Appointment, User> {
 
         try (Connection conn = dataSource.getConnection();
              PreparedStatement statement = conn.prepareStatement(
-                     "INSERT INTO APPOINTMENTS VALUES (?,?,?,?,?,?,?,?,?,?)")) {
-            statement.setInt(1, appointment.id());
-            statement.setString(2, appointment.title());
-            statement.setString(3, appointment.description());
-            statement.setString(4, appointment.location());
-            statement.setString(5, appointment.type());
-            statement.setTimestamp(6, Timestamp.valueOf(appointment.start()));
-            statement.setTimestamp(7, Timestamp.valueOf(appointment.end()));
-            statement.setInt(8, appointment.customerId());
-            statement.setInt(9, appointment.userId());
-            statement.setInt(10, appointment.contactId());
-            return statement.execute();
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean update(Appointment appointment, User currentUser) throws SQLException {
-
-        //TODO:  insert Last_Update (TIMESTAMP) field
-
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement statement = conn.prepareStatement(
-                     "UPDATE APPOINTMENTS SET " +
-                             "Title = ?, " +
-                             "Description = ?, " +
-                             "Location = ?, " +
-                             "Type = ?, " +
-                             "Start = ? " +
-                             "End = ? " +
-                             "Customer_ID = ? " +
-                             "User_ID = ? " +
-                             "Contact_ID = ? " +
-                             "WHERE Appointment_ID = ?")) {
+                 """
+                         INSERT INTO client_schedule.appointments\s
+                         (Title, Description, Location, Type, Start, End,\s
+                         Customer_ID, User_ID, Contact_ID,\s
+                         Created_By, Last_Updated_By, Create_Date, Last_Update)\s
+                         VALUES (?,?,?,?,?,?,?,?,?,?,?,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
+                         """)) {
             statement.setString(1, appointment.title());
             statement.setString(2, appointment.description());
             statement.setString(3, appointment.location());
@@ -123,7 +108,46 @@ public class AppointmentDaoImpl extends DataAccessObject<Appointment, User> {
             statement.setInt(7, appointment.customerId());
             statement.setInt(8, appointment.userId());
             statement.setInt(9, appointment.contactId());
-            statement.setInt(10, appointment.id());
+            statement.setString(10, user.name());
+            statement.setString(11, user.name());
+
+            return statement.execute();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean update(Appointment appointment, User user) throws SQLException {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement statement = conn.prepareStatement(
+                 """
+                         UPDATE client_schedule.appointments SET\s
+                         Title = ?,\s
+                         Description = ?,\s
+                         Location = ?,\s
+                         Type = ?,\s
+                         Start = ?,\s
+                         End = ?,\s
+                         Customer_ID = ?,\s
+                         User_ID = ?,\s
+                         Contact_ID = ?,\s
+                         Last_Updated_By = ?\s
+                         Last_Update = CURRENT_TIMESTAMP,\s
+                         WHERE Appointment_ID = ?
+                         """)) {
+            statement.setString(1, appointment.title());
+            statement.setString(2, appointment.description());
+            statement.setString(3, appointment.location());
+            statement.setString(4, appointment.type());
+            statement.setTimestamp(5, Timestamp.valueOf(appointment.start()));
+            statement.setTimestamp(6, Timestamp.valueOf(appointment.end()));
+            statement.setInt(7, appointment.customerId());
+            statement.setInt(8, appointment.userId());
+            statement.setInt(9, appointment.contactId());
+            statement.setString(10, user.name());
+            statement.setInt(11, appointment.id());
             return statement.executeUpdate() > 0;
         }
     }
@@ -135,7 +159,7 @@ public class AppointmentDaoImpl extends DataAccessObject<Appointment, User> {
     public boolean delete(int id) throws SQLException {
         try (Connection conn = dataSource.getConnection();
              PreparedStatement statement = conn.prepareStatement(
-                     "DELETE FROM APPOINTMENTS WHERE Appointment_ID = ?")) {
+                 "DELETE FROM client_schedule.appointments WHERE Appointment_ID = ?")) {
             statement.setInt(1, id);
             return statement.executeUpdate() > 0;
         }
@@ -146,9 +170,6 @@ public class AppointmentDaoImpl extends DataAccessObject<Appointment, User> {
      */
     @Override
     protected Appointment createRecordFromResultSet(ResultSet resultSet) throws SQLException {
-
-        // TODO:  get contact name and email from table JOIN
-
         return new Appointment(
                 resultSet.getInt("Appointment_ID"),
                 resultSet.getString("Title"),
@@ -158,6 +179,7 @@ public class AppointmentDaoImpl extends DataAccessObject<Appointment, User> {
                 resultSet.getTimestamp("Start").toLocalDateTime(),
                 resultSet.getTimestamp("End").toLocalDateTime(),
                 resultSet.getInt("Customer_ID"),
+                resultSet.getString("Customer_Name"),
                 resultSet.getInt("User_ID"),
                 resultSet.getInt("Contact_ID"),
                 resultSet.getString("Contact_Name"),
