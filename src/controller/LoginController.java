5a8493cf58179f79477a8d70c1ab0d07dd536a10
Joseph Curtis <jcur175@wgu.edu>
2022-06-07 21:36:52 -0700

    removed unnecessary import

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -19,7 +19,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyCode;
 import javafx.stage.Modality;
 import model.User;
 import utility.BlankInputException;
